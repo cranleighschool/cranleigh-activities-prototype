03c8bdc96db66f191d223447f842eac8c0e60c43
Fred Bradley <hello@fredbradley.co.uk>
2016-05-20 09:03:45 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,238 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <header id="masthead">
+	        <div class="container">
+		        <div class="row">
+			        <div class="col-md-4">
+						<img id="logo-img" src="https://parents.cranleigh.org/files/2016/04/cranleigh-logo-horiz.png" class="img-responsive" />
+			        </div>
+		        </div>
+	        </div>
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+
+					<div id="navbar" class="navbar-collapse collapse">
+						<ul class="nav nav-justified">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="#">Sports Clubs</a></li>
+							<li><a href="#">Swimming Academy</a></li>
+							<li><a href="#">Equestrian</a></li>
+							<li><a href="#">Something</a></li>
+							<li><a href="#">Something More</a></li>
+							<li><a href="#">Last thing</a></li>
+						</ul>
+					</div><!--/.navbar-collapse -->
+				</div>
+			</nav>
+		</header>
+
+		<div id="site-content" class="container">
+			<div class="row">
+				<div class="col-md-8">
+					<div class="row">
+						<div class="col-md-12">
+							<div id="myCarousel" class="carousel slide" data-ride="carousel">
+								<!-- Wrapper for slides -->
+								<div class="carousel-inner">
+
+									<div class="item active">
+										<img src="http://placehold.it/1200x400/cccccc/ffffff">
+										<div class="carousel-caption">
+											<h3>Headline</h3>
+											<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. <a href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank" class="label label-danger">Bootstrap 3 - Carousel Collection</a></p>
+										</div>
+									</div><!-- End Item -->
+
+									<div class="item">
+										<img src="http://placehold.it/1200x400/999999/cccccc">
+										<div class="carousel-caption">
+											<h3>Headline</h3>
+											<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. <a href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank" class="label label-danger">Bootstrap 3 - Carousel Collection</a></p>
+										</div>
+									</div><!-- End Item -->
+
+									<div class="item">
+										<img src="http://placehold.it/1200x400/dddddd/333333">
+										<div class="carousel-caption">
+											<h3>Headline</h3>
+											<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. <a href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank" class="label label-danger">Bootstrap 3 - Carousel Collection</a></p>
+										</div>
+									</div><!-- End Item -->
+
+									<div class="item">
+										<img src="http://placehold.it/1200x400/999999/cccccc">
+										<div class="carousel-caption">
+											<h3>Headline</h3>
+											<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+										</div>
+									</div><!-- End Item -->
+
+								</div><!-- End Carousel Inner -->
+
+
+								<ul class="nav nav-pills nav-justified">
+									<li data-target="#myCarousel" data-slide-to="0" class="active"><a href="#">About<small>Lorem ipsum dolor sit</small></a></li>
+									<li data-target="#myCarousel" data-slide-to="1"><a href="#">Projects<small>Lorem ipsum dolor sit</small></a></li>
+									<li data-target="#myCarousel" data-slide-to="2"><a href="#">Portfolio<small>Lorem ipsum dolor sit</small></a></li>
+									<li data-target="#myCarousel" data-slide-to="3"><a href="#">Services<small>Lorem ipsum dolor sit</small></a></li>
+								</ul>
+
+
+							</div><!-- End Carousel -->
+						</div>
+					</div>
+
+					<div class="row welcome">
+						<div class="col-md-12">
+							<h3>Welcome to Cranleigh Activities</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lacus orci, dictum ut tellus in, maximus interdum leo. Pellentesque non eleifend quam. Nam facilisis fermentum risus, eget luctus erat posuere eget. Vivamus posuere non ante vitae dictum. Aliquam efficitur egestas nisi placerat vestibulum. Proin fermentum, velit nec dictum mattis, augue sem dictum erat, quis fringilla est est in dui. Sed leo justo, rhoncus sit amet ligula ut, blandit ultricies eros.</p>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-6">
+							<div class="widget">
+								<h3>Widget Title</h3>
+								<p>Some content</p>
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="widget">
+								<h3>Widget Title</h3>
+								<p>Some content</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="widget">
+						<h3>Widget Title</h3>
+						<p>Some content</p>
+					</div>
+					<div class="widget">
+						<h3>Widget Title</h3>
+						<p>Some content</p>
+					</div>
+					<div class="widget">
+						<h3>Widget Title</h3>
+						<p>Some content</p>
+					</div>
+				</div>
+
+			</div>
+		</div><!-- #site-content -->
+
+
+
+		<footer id="footer-wrapper" role="contentinfo">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3">
+						<h4>Address</h4>
+						<address>
+							Cranleigh School, Horseshoe Lane, Cranleigh, Surrey GU6 8QQ
+						</address>
+						<p>Telephone: 01483 273666<br>
+							Email: reception@cranleigh.org</p>
+					</div>
+					<div class="col-sm-3">
+						<h4>Social Media</h4>
+						<ul class="list-unstyled">
+							<li><img src="http://www.cranleighdev.org/prototypes/img/facebook-grey.png" alt="facebook"> Like us on Facebook</li>
+							<li><img src="http://www.cranleighdev.org/prototypes/img/twitter-grey.png" alt="twitter"> Follow us on Twitter</li>
+							<li><img src="http://www.cranleighdev.org/prototypes/img/pinterest-grey.png" alt="pinterest"> Watch us on Pinterest</li>
+							<li><img src="http://www.cranleighdev.org/prototypes/img/vimeo-grey.png" alt="vimeo"> View us on Vimeo</li>
+						</ul>
+					</div>
+					<div class="col-sm-3">
+						<h4>Most Popular</h4>
+						<ul>
+							<li><a href="subject.php">History</a></li>
+							<li><a href="default-page.php">Default Page</a></li>
+							<li>Link</li>
+							<li>Link</li>
+						</ul>
+					</div>
+					<div class="col-sm-3">
+						<img class="img-responsive" src="http://www.cranleighdev.org/prototypes/img/cranleigh-logo-horiz.png" alt="Cranleigh school logo">
+					</div>
+				</div>
+			</div>
+		</footer>
+<?php /*
+		<footer id="footer-wrapper">
+			<div class="footer-content container">
+			<!-- Example row of columns -->
+				<div class="row">
+					<div class="col-md-4">
+						<h2>Heading</h2>
+						<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+						<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+					</div>
+
+					<div class="col-md-4">
+						<h2>Heading</h2>
+						<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+						<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+					</div>
+					<div class="col-md-4">
+						<h2>Heading</h2>
+						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+						<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+					</div>
+				</div>
+			</div>
+		</footer> */ ?>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
+
